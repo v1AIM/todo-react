3f7e99961910c2bcaaeccf6fdb3d70d563df3e82
Amr.desktop <b1063629@gmail.com>
2024-07-24 22:17:11 +0300

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/todos";
 
-//TODO: Complete the App component
-
 function App() {
   return (
     <main className="py-10  h-screen space-y-5">
