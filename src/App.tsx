7223de9098c86b13fa32708898a1ac68937d3cdf
Amr.desktop <Amr@gmail.com>
2024-07-24 22:07:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import TodoItem from "./components/TodoItem";
+import { dummyData } from "./data/todos";
+
+function App() {
+  return (
+    <main className="py-10  h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+        <div className="space-y-2">
+          {dummyData.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../types/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Like This Video 👍",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Suscirbe For More Videos 🚀",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Leave a Comment 💬",
+    completed: true,
+  },
+];
