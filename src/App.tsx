367a9a381d67185cc25fc9d61cbe8465fba0f945
Amr.desktop <b1063629@gmail.com>
2024-07-25 10:34:22 +0300

    chore: Refactor App component to use hooks and add functionality for adding, completing, and deleting todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
-import TodoItem from "./components/TodoItem";
+import { useState } from "react";
 import { dummyData } from "./data/todos";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState(dummyData);
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
+    // setTodos((prevTodos) =>
+    //   prevTodos.map((todo) => {
+    //     if (todo.id === id) {
+    //       return { ...todo, completed };
+    //     }
+    //     return todo;
+    //   })
+    // );
+  }
+
+  function addTodo(title: string) {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+    ]);
+  }
+
+  function deleteTodo(id: number) {
+    setTodos((prevTodos) => prevTodos.filter((todos) => todos.id !== id));
+  }
+
   return (
-    <main className="py-10  h-screen space-y-5">
+    <main className="py-10  h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
-        <div className="space-y-2">
-          {dummyData.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
-          ))}
-        </div>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList
+          todos={todos}
+          onCompletedChange={setTodoCompleted}
+          onDelete={deleteTodo}
+          // onDelete={(id) =>
+          //   setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
+          // }
+        />
       </div>
     </main>
   );
