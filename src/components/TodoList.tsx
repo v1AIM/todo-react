2d45e0b9dd9092a4158c13026d0ccb23b5d21184
Amr.desktop <b1063629@gmail.com>
2024-07-25 10:34:36 +0300

    feat: Add TodoList component for displaying a list of todos

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,40 @@
+import { Todo } from "../types/todo";
+import TodoItem from "./TodoItem";
+
+interface TodoListProps {
+  todos: Todo[];
+  onCompletedChange: (id: number, completed: boolean) => void;
+  onDelete: (id: number) => void;
+}
+
+export default function TodoList({
+  todos,
+  onCompletedChange,
+  onDelete,
+}: TodoListProps) {
+  const todosSorted = todos.sort((a, b) => {
+    if (a.completed === b.completed) {
+      return b.id - a.id;
+    }
+    return a.completed ? 1 : -1;
+  });
+  return (
+    <>
+      <div className="space-y-2">
+        {todosSorted.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            onCompletedChange={onCompletedChange}
+            todo={todo}
+            onDelete={onDelete}
+          />
+        ))}
+      </div>
+      {todos.length === 0 && (
+        <p className="text-center text-sm text-gray-500">
+          No todos found. Add new above
+        </p>
+      )}
+    </>
+  );
+}
